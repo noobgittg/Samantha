1b9eac51011e63d951e7a3be89e718aaa9d3eb9f
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2025-06-23 17:26:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,7 +5,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
-from database.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -22,9 +21,6 @@ async def inline_users(query: InlineQuery):
 
 @Client.on_inline_query()
 async def answer(bot, query):
-    """Show search results for given inline query"""
-    chat_id = await active_connection(str(query.from_user.id))
-    
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
@@ -32,12 +28,11 @@ async def answer(bot, query):
                            switch_pm_parameter="hehe")
         return
 
-    invite_links = await is_subscribed(bot, query=query)
-    if AUTH_CHANNEL and len(invite_links) >= 1:
+    if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
-            cache_time=0,
-            switch_pm_text='You have to subscribe my channel to use the bot',
-            switch_pm_parameter="subscribe")
+                           cache_time=0,
+                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_parameter="subscribe")
         return
 
     results = []
@@ -51,20 +46,15 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset, total = await get_search_results(
-                                                  chat_id,
-                                                  string,
-                                                  file_type=file_type,
-                                                  max_results=10,
-                                                  offset=offset)
-
+    files, next_offset, total = await get_search_results(string, file_type=file_type, max_results=10, offset=offset)
+                                                 
     for file in files:
         title=file.file_name
         size=get_size(file.file_size)
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
@@ -94,7 +84,7 @@ async def answer(bot, query):
         except Exception as e:
             logging.exception(str(e))
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} No results'
+        switch_pm_text = f'{emoji.CROSS_MARK} No Results'
         if string:
             switch_pm_text += f' for "{string}"'
 
@@ -106,13 +96,7 @@ async def answer(bot, query):
 
 
 def get_reply_markup(query):
-    buttons = [
-        [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
-        ]
-        ]
+    buttons = [[InlineKeyboardButton('⟳ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query)]]
     return InlineKeyboardMarkup(buttons)
-
-
 
 

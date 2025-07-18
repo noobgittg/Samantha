2338b22db3eed87d47cd341b7174cdbf3d6571ad
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2025-07-18 19:55:49 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from info import *
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -21,8 +21,6 @@ async def inline_users(query: InlineQuery):
 
 @Client.on_inline_query()
 async def answer(bot, query):
-    """Show search results for given inline query"""
-    
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
@@ -30,11 +28,12 @@ async def answer(bot, query):
                            switch_pm_parameter="hehe")
         return
 
-    if AUTH_CHANNEL and not await is_subscribed(bot, query):
+    invite_links = await is_subscribed(bot, query=query)
+    if AUTH_CHANNEL and len(invite_links) >= 1:
         await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
-                           switch_pm_parameter="subscribe")
+            cache_time=0,
+            switch_pm_text='You have to subscribe my channel to use the bot',
+            switch_pm_parameter="subscribe")
         return
 
     results = []
@@ -103,7 +102,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton(' 🔍 Search Again', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)

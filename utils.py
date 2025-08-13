64f24388d101d0d6deae7a87ce115aeb393dae2c
Noob <akhilkrishnaus1234@gmail.com>
2025-08-13 17:59:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -72,7 +72,6 @@ async def is_subscribed(bot, query=None, userid=None):
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
-        # https://t.me/GetTGLink/4183
         query = (query.strip()).lower()
         title = query
         year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
@@ -148,7 +147,6 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
 async def broadcast_messages(user_id, message):
     try:

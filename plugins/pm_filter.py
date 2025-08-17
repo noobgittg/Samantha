3f9346235e6cca9fb1b5aba609117b1631a80437
Noob <akhilkrishnaus1234@gmail.com>
2025-08-17 18:08:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -35,10 +35,11 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.group | filters.private & filters.text & filters.incoming) #GIVE FILTER IN PM BRO IDEA OF GOUTHAM SER
+@Client.on_message(filters.group & filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
-    k = await auto_ffilter(client, message)
-    
+    k = await manual_filters(client, message)
+    if k == False:
+        await auto_filter(client, message)        
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):

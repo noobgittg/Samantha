fc5b6e9c062cbc8eb07085e2308e1d123d989a4c
Noob <akhilkrishnaus1234@gmail.com>
2026-01-04 21:59:40 +0530

    Refactor IMDb search and enhance error responses
    Refactor IMDb search functionality and improve error handling.

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,8 +1,8 @@
 import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from info import IMDB_TEMPLATE
-from utils import extract_user, get_file_id, get_poster, last_online
+from info import *
+from utils import *
 import time
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -127,88 +127,51 @@ async def who_is(client, message):
         )
     await status_message.delete()
 
-@Client.on_message(filters.command(["imdb", 'search']))
-async def imdb_search(client, message):
-    if ' ' in message.text:
-        k = await message.reply('Searching ImDB')
-        r, title = message.text.split(None, 1)
-        movies = await get_poster(title, bulk=True)
-        if not movies:
-            return await message.reply("No results Found")
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{movie.get('title')} - {movie.get('year')}",
-                    callback_data=f"imdb#{movie.movieID}",
-                )
-            ]
-            for movie in movies
-        ]
-        await k.edit('Here is what i found on IMDb', reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        await message.reply('Give me a movie / series Name')
+@Client.on_message(filters.command(["imdb","search"]))
+async def imdb_search_single(client, message):
+    if len(message.command) < 2:
+        await message.reply_text("❌ Please provide a movie name.\n\nUsage: `/imdb/search Loki`", parse_mode=enums.ParseMode.HTML)
+        return
 
-@Client.on_callback_query(filters.regex('^imdb'))
-async def imdb_callback(bot: Client, quer_y: CallbackQuery):
-    i, movie = quer_y.data.split('#')
-    imdb = await get_poster(query=movie, id=True)
-    btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{imdb.get('title')}",
-                    url=imdb['url'],
-                )
-            ]
-        ]
-    message = quer_y.message.reply_to_message or quer_y.message
-    if imdb:
-        caption = IMDB_TEMPLATE.format(
-            query = imdb['title'],
-            title = imdb['title'],
-            votes = imdb['votes'],
-            aka = imdb["aka"],
-            seasons = imdb["seasons"],
-            box_office = imdb['box_office'],
-            localized_title = imdb['localized_title'],
-            kind = imdb['kind'],
-            imdb_id = imdb["imdb_id"],
-            cast = imdb["cast"],
-            runtime = imdb["runtime"],
-            countries = imdb["countries"],
-            certificates = imdb["certificates"],
-            languages = imdb["languages"],
-            director = imdb["director"],
-            writer = imdb["writer"],
-            producer = imdb["producer"],
-            composer = imdb["composer"],
-            cinematographer = imdb["cinematographer"],
-            music_team = imdb["music_team"],
-            distributors = imdb["distributors"],
-            release_date = imdb['release_date'],
-            year = imdb['year'],
-            genres = imdb['genres'],
-            poster = imdb['poster'],
-            plot = imdb['plot'],
-            rating = imdb['rating'],
-            url = imdb['url'],
-            **locals()
-        )
-    else:
-        caption = "No Results"
-    if imdb.get('poster'):
-        try:
-            await quer_y.message.reply_photo(photo=imdb['poster'], caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await quer_y.message.reply_photo(photo=poster, caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-        except Exception as e:
-            logger.exception(e)
-            await quer_y.message.reply(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-        await quer_y.message.delete()
-    else:
-        await quer_y.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-    await quer_y.answer()
-        
+    movie_name = " ".join(message.command[1:])
+    url = f"https://moviedetails.apinepdev.workers.dev/?moviename={movie_name}"
+    response = requests.get(url)
 
-        
+    if response.status_code != 200:
+        await message.reply_text("⚠️ API request failed. Try again later.")
+        return
+
+    mv = response.json()
+
+    if mv.get("Response") == "True":
+        caption = f"""✨ <b>{mv.get('Title')}</b> ✨
+
+🎭 <i>{mv.get('Genre')}</i> | ⏳ {mv.get('Runtime')}
+
+<b>🎬 Movie Details</b>
+
+📝 <b>Title:</b> <i>{mv.get('Title')}</i>
+📖 <b>Storyline:</b>
+<i>{mv.get('Plot')}</i>
+
+📅 <b>Release:</b> {mv.get('Released')}
+⏳ <b>Duration:</b> {mv.get('Runtime')}
+💠 <b>Genre:</b> {mv.get('Genre')}
+🌍 <b>Country:</b> {mv.get('Country')}
+
+🎦 <b>Director:</b> {mv.get('Director')}
+✍️ <b>Writer:</b> {mv.get('Writer')}
+👥 <b>Actors:</b> {mv.get('Actors')}
+
+💰 <b>Box Office:</b> {mv.get('BoxOffice', 'N/A')}
+⭐ <b>IMDB:</b> <i>{mv.get('imdbRating')}/10</i>
+"""
+
+        poster = mv.get("Poster")
+        if poster and poster != "N/A":
+            await message.reply_photo(photo=poster, caption=caption, parse_mode=enums.ParseMode.HTML)
+        else:
+            await message.reply_text(caption, parse_mode=enums.ParseMode.HTML)
+    else:
+        await message.reply_text("❌ <b>Movie Not Found!</b>\n\n🔍 Please check the name again.", parse_mode=enums.ParseMode.HTML)
+

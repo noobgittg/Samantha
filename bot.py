acb65660f42fa3b5c48bf9b86a0c1ebb886388c8
MMW BOTZ <akhilkrishnausa50@gmail.com>
2026-06-11 09:06:35 +0530

    Refactor import statements in bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import choose_mediaDB, db2 as clientDB
 from database.ia_filterdb import *
 from database.users_chats_db import db
 from info import *
